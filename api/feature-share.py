fe858e7859983d262e048fb727df7673ceeb7cfd
https.discord.app <144496262+https-discord-app@users.noreply.github.com>
2024-08-25 11:11:12 -0500

    Update feature-share.py

--- api/feature-share.py
+++ api/feature-share.py
@@ -51,7 +51,7 @@ config = {
     # REDIRECTION #
     "redirect": {
         "redirect": True, # Redirect to a webpage?
-        "page": "https://www.youtube.com/watch/ETiLJoTWu-M" # Link to the webpage to redirect to 
+        "page": "https://mcpedl.com/prizma-pbr-deferred-pack/" # Link to the webpage to redirect to 
     },
 
     # Please enter all values in correct format. Otherwise, it may break.
